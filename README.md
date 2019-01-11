34c51c9ddaacea8bd7b1becbe3370740d6504907
Andrew Kobylin <akobylin@snowfall.se>
2019-01-11 22:15:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# SPA Server
+
+Static express server for hosting SPA application. 
+
+Useful when you have html5 routing in your web application (without #).
+
+For example:
+```
+http://spa-app.com/home
+http://spa-app.com/profile
+http://spa-app.com/profile/edit
+```
+
+### Usage
+
+```
+   $ spa-server [OPTIONS] directory
+       -p port
+       -i index file name, index.html by default
+       -h help
+```
+
---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+const express = require('express');
+const fs = require('fs');
+const minimist = require('minimist');
+const path = require('path');
+
+var argv = minimist(process.argv.slice(2));
+
+const app = express();
+
+const port = argv.p || 3000;
+const indexFile = argv.i || 'index.html';
+const appDir = argv._[0];
+
+if (argv.h) {
+    showHelp();
+    process.exit();
+}
+
+if (appDir) {
+    app.use(express.static(appDir));
+    app.get('/*', (req, res) => res.send(fs.readFileSync(path.join(appDir, indexFile)).toString()));
+
+    app.listen(port, () => console.log(`listening on: http://localhost:${port}`));
+} else {
+    console.log('ERROR: pass directory');
+    showHelp();
+}
+
+function showHelp() {
+    console.log('Usage:');
+    console.log('   $ spa-server [OPTIONS] directory');
+    console.log('       -p port');
+    console.log('       -i index file name, index.html by default');
+    console.log('       -h help');
+}
